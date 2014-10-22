7e096fac92823dd1f72a1bca1c2085cbf6b0b2ae
Tô Hữu Quân <tohuuquan@gmail.com>
2014-10-22 14:57:16 +0700

    Seperate router implementation

---
+++ app.js
@@ -0,0 +1,34 @@
+/* Server */
+var express = require('express'),
+    http    = require('http'),
+    path    = require('path'),
+    argv    = require('optimist').argv;
+
+/* Config */
+var port = argv.port || 5000;
+
+/* All environments */
+var app = express();
+var morgan  = require('morgan');
+var bodyParser = require('body-parser');
+var methodOverride = require('method-override');
+
+app.use(morgan('combined'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(methodOverride());
+
+/* Definition */
+app.all('/v1/*', function (req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	return next();
+});
+
+var router = require('./controllers/v1/router');
+app.use('/v1', router());
+
+
+/* Create server */
+app.listen(port, function () {
+    console.log("Magic happens on port", port);
+});
