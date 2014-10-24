5f534485f35c2fa2c96b0ffc26de0606b0410d4c
kantlove <minhthai40@gmail.com>
2014-10-24 09:25:22 +0700

    Complete swagger files

--- swagger/router.js
+++ swagger/router.js
@@ -11,62 +11,59 @@ var resources = require("./resources.js");
 // methods, the header `api_key` OR query param `api_key` must be equal
 // to the string literal `special-key`.  All other HTTP ops are A-OK
 swagger.addValidator(
-  function validate(req, path, httpMethod) {
-    return true;
-  }
+    function validate(req, path, httpMethod) {
+        return true;
+    }
 );
 
 var models = require("./models.js");
 
 // Add models and methods to swagger
 swagger.addModels(models)
-  .addGet(resources.getFunction)
-  .addPost(resources.postFunction)
-  .addPut(resources.putFunction)
-  .addDelete(resources.deleteFunction);
+.addGet(resources.getFunction)
+.addPost(resources.postFunction)
+.addPut(resources.putFunction)
+.addDelete(resources.deleteFunction);
 
 swagger.configureDeclaration("user", {
-  description : "Testing functions",
-  authorizations : ["oauth2"],
-  produces: ["application/json"]
+    description : "Testing functions",
+    authorizations : ["oauth2"],
+    produces: ["application/json"]
 });
 
 // set api info
 swagger.setApiInfo({
-  title: "DSN API",
-  description: "API Document for DSN",
-  termsOfServiceUrl: "http://helloreverb.com/terms/",
-  contact: "minhthai40@gmail.com",
-  license: "Apache 2.0",
-  licenseUrl: "http://www.apache.org/licenses/LICENSE-2.0.html"
+    title: "Dream Social Network API Documentation",
+    description: "API Document for Dream Social Network",
 });
 
 swagger.setAuthorizations({
-  apiKey: {
-    type: "apiKey",
-    passAs: "header"
-  }
+    apiKey: {
+        type: "apiKey",
+        passAs: "header"
+    }
 });
 
 // Configures the app's base path and api version.
 swagger.configureSwaggerPaths("", "api-docs", "")
-//swagger.configure("http://localhost:8002", "1.0.0");
+swagger.configure("http://localhost:5000", "1.0.0");
 
 // Serve up swagger ui at /docs via static route
 var docs_handler = express.static(__dirname + '/swagger-ui/');
+
 router.get(/^\/docs(\/.*)?$/, function(req, res, next) {
-  if (req.url === '/docs') { // express static barfs on root url w/o trailing slash
-    res.writeHead(302, { 'Location' : req.url + '/docs/' });
-    res.end();
-    return;
-  }
-  // take off leading /docs so that connect locates file correctly
-  req.url = req.url.substr('/docs'.length);
-  return docs_handler(req, res, next);
+    if (req.url === '/docs') { // express static barfs on root url w/o trailing slash
+        res.writeHead(302, { 'Location' : req.url + '/docs/' });
+        res.end();
+        return;
+    }
+    // take off leading /docs so that connect locates file correctly
+    req.url = req.url.substr(5);
+    return docs_handler(req, res, next);
 });
 
 router.use(function(err, req, res, next){
-  res.send(err.status, err.message);
+    res.send(err.status, err.message);
 });
 
 // Start the server on port 8002
